01f552ea507fcbda2c196cb6558a6594358254d8
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2023-08-16 17:19:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,37 @@
+# Necessary Imports
 import streamlit as st
+# Standard libraries
+import time
+import os
+import base64
+
+# Data manipulation and analysis
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
+
+# Plotting and visualization
+import plotly.graph_objects as go
+import plotly.express as px
+import imageio.v2 as imageio_v2
+
+# Display utilities
+from IPython.display import display, HTML
+
+# External libraries
+import requests
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline, logging
+logging.set_verbosity_error()
+
+# Serialization
+import pickle
+
+# OpenAI
+import openai
+# Set up OpenAI API key
+openai_key = st.secrets["OPENAI_KEY"]
+
 st.write("Hello, Streamlit!")
